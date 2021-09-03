61f5e5b89b82db58691b8de1839b20991a07f6c1
Jozef Ďurina <jozef.durina@gmail.com>
2021-09-03 11:49:06 +0200

    uprava parametrov pre vykreslenie

--- sinus.py
+++ sinus.py
@@ -4,7 +4,7 @@ def drawSinus():
     for angle_in_deg in range(0, 361, 10):
         angle_in_rad = math.radians ( angle_in_deg )
         sinus = math.sin ( angle_in_rad )
-        odskok = int ( 40 + 35 * sinus )
+        odskok = round ( 38 + 37 * sinus )
         print ( ' ' * odskok + 'SINUS')
 
 drawSinus()
