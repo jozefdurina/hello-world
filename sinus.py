260777d33935b2a9b6abe09259f15c4dc2a302a9
Jozef Ďurina <jozef.durina@gmail.com>
2021-09-03 10:51:05 +0200

    oddelenie vykreslenia do samostatnej funkcie

--- sinus.py
+++ sinus.py
@@ -1,7 +1,10 @@
 import math
 
-for angle_in_deg in range(0, 361, 10):
-    angle_in_rad = math.radians ( angle_in_deg )
-    sinus = math.sin ( angle_in_rad )
-    odskok = int ( 40 + 35 * sinus )
-    print ( ' ' * odskok + 'SINUS')
+def drawSinus():
+    for angle_in_deg in range(0, 361, 10):
+        angle_in_rad = math.radians ( angle_in_deg )
+        sinus = math.sin ( angle_in_rad )
+        odskok = int ( 40 + 35 * sinus )
+        print ( ' ' * odskok + 'SINUS')
+
+drawSinus()
