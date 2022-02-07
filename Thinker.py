d90d1bcecdf9445b54c47ea673053ba4490f92c6
Dlabka <stevodlabka@gmail.com>
2022-02-07 22:45:23 +0100

    pondelkovy postup ...

--- Thinker.py
+++ Thinker.py
@@ -2,11 +2,22 @@ from tkinter import *
 from tkinter import ttk
 import tkinter as tk
 from unicodedata import name
+#from tkinter import messagebox
+
+#definicie vypoctovych map
+
+portaly= {
+    "PORTÁL, kotevný"               :   [[-12,18.799],[0,18.799],[12,18.799],[-6,31.789],[6,31.789]],
+    "PORTÁL, kotevný rozkročený"    :   [[1,2],[3,4],[5,6],[7,8],[9,10]]
+}
+
 
 root = Tk()
+root.geometry("600x500")
+root.title("Výpočet parametrov vedení")
 #root2 = Tk()                             #Druhý rámík
 frm = ttk.Frame(root, padding=10)
-#frm2 = ttk.Frame(root, padding=10)
+#frm2 = ttk.Frame(root2, padding=10)
 
 frm.grid(row=0)
 #frm2.grid(row=1)
@@ -22,40 +33,34 @@ ttk.Label(frm, text="Metóda výpočtu").grid(column=0, row=6)
 
 
 #ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-#ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=1)
-#ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=2)
-#ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=3)
-#ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=4)
-#ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=5)
-#ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=6)
-#ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=7)
-ttk.Button(text="Vypočítaj").grid()
-ttk.Button(text="Vypočítaj 2").grid()
-
-Portál="akalsk"
 
 
-Vodič = "aa"
+vypocitaj = ttk.Button(text="Vypočítaj",).grid()
+vypocitaj2 = ttk.Button(text="Vypočítaj 2").grid()
+
+
 labelTop = tk.Label(frm, text = "Počet vodičov vo zväzku")
 labelTop.grid(column=0, row=7)
 
 
-Portál = ttk.Combobox(frm, values=[
-                                    "Kotevný", 
-                                    "Kotevný rozkročený",
-                                    "Kotevný typ 1",
-                                    "Kotevný typ 2 užší povodný",
-                                    "Kotevný typ 3 širší povodný",
-                                    "Nosný uzky",
-                                    "Nosný rozkročený"])
-#print(dict(Portál))                           #čo je dict??                
-Portál.grid(column=2, row=0)
-Portál.current(0)
+PortálCombo = ttk.Combobox(frm, state="readonly", values=list(portaly.keys()))
+              
+PortálCombo.grid(column=2, row=0)
+#ortál.current(0)
 
-print(Portál.current(), Portál.get())
+#print(Portál.current(), Portál.get())
 
+def debug_pressed():
+    messagebox.showinfo("debug", PortálCombo["values"])
 
-ttk.Combobox(frm, values=["185  AlFe 3",
+debug = ttk.Button(text="Debug", command=debug_pressed).grid()
+
+def checkcmbo():
+
+     if PortálCombo.get() == "Kotevný":
+         messagebox.showinfo("What user choose", "you choose prova")
+
+fazovyVodicCombo = ttk.Combobox(frm, values=["185  AlFe 3",
 "185  AlFe 6",
 "185  AlFe 3",
 "240  AlFe 6",
@@ -64,7 +69,9 @@ ttk.Combobox(frm, values=["185  AlFe 3",
 "450  AlFe 6",
 "450  AlFe 8",
 "500  AlFe 8",
-"670  AlFe 8",]).grid(column=2, row=1)
+"670  AlFe 8",])
+
+fazovyVodicCombo.grid(column=2, row=1)
 
 
 ttk.Combobox(frm, values=["185  AlFe 3",
